33482af8d19c7566f7b12ea98d6d9fc3248e49e6
moreveal <morrisreveal@gmail.com>
2026-09-11 19:15:16 +0400

    fix(webidl): validate and owner-dispatch History and Storage operations
    Signed-off-by: moreveal <morrisreveal@gmail.com>

---
+++ internal/browser/history_storage_binding_test.go
@@ -0,0 +1,9 @@
+//go:build windows && amd64
+
+package browser
+
+import "testing"
+
+func TestHistoryStorageBindingsMatchFrozenChrome(t *testing.T) {
+	documentAllOracle(t, "history_storage_binding")
+}
